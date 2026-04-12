30fc631d5b6a299bc343de1f6133c53244fe5ac4
Falzafar <naufalafiqmm@gmail.com>
2026-04-12 10:20:02 +0700

    s

--- soal1.c
+++ soal1.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
-int main(void) {
+int main() {
+    int izin, suhu, radiasi, jam;
+    scanf("%d %d %d %d", &izin, &suhu, &radiasi, &jam);
+
+    if (radiasi >= 6) {
+        printf("TOLAK\n");
+    } else if (suhu >= 390) {
+        printf("KARANTINA\n");
+    } else if (izin == 1 && (jam < 6 || jam > 20)) {
+        printf("TOLAK\n");
+    } else if (izin == 1) {
+        printf("MASUK\n");
+    } else if (izin == 2 && radiasi <= 2 && jam >= 8 && jam <= 18) {
+        printf("MASUK\n");
+    } else if (izin == 2) {
+        printf("PEMERIKSAAN\n");
+    } else if (izin == 3 && radiasi == 0 && suhu < 380) {
+        printf("MASUK\n");
+    } else {
+        printf("TOLAK\n");
+    }
+
     return 0;
 }
