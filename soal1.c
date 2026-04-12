475bd2d11c925488f112c4e53c1b778e4545cfbf
sastrasandyakala <sastrasandyakala@gmail.com>
2026-04-13 06:58:56 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -11,9 +11,9 @@ int main() {
     } else if (izin == 1 && (jam < 6 || jam > 20)) {
         printf("TOLAK\n");
     } else if (izin == 1) {
-        printf("MASUK\n");
+        printf("MASUK");
     } else if (izin == 2 && radiasi <= 2 && jam >= 8 && jam <= 18) {
-        printf("MASUK\n");
+        printf("MASUK");
     } else if (izin == 2) {
         printf("PEMERIKSAAN\n");
     } else if (izin == 3 && radiasi == 0 && suhu < 380) {
