9ea3119d71e9779f2b0e52b052187bf039547abd
sastrasandyakala <sastrasandyakala@gmail.com>
2026-04-13 07:25:14 +0700

    a

--- soal1.c
+++ soal1.c
@@ -5,7 +5,7 @@ int main() {
     scanf("%d %d %d %d", &izin, &suhu, &radiasi, &jam);
 
     if (radiasi >= 6) {
-        printf("TOLAK ");
+        printf("TOLAK\n");
     } else if (suhu >= 390) {
         printf("KARANTINA\n");
     } else if (izin == 1 && (jam < 6 || jam > 20)) {
@@ -13,7 +13,7 @@ int main() {
     } else if (izin == 1) {
         printf("MASUK\n");
     } else if (izin == 2 && radiasi <= 2 && jam >= 8 && jam <= 18) {
-        printf("MASUK");
+        printf("MASUK\n");
     } else if (izin == 2) {
         printf("PEMERIKSAAN");
     } else if (izin == 3 && radiasi == 0 && suhu < 380) {
